4f347a59664ae6b35c273de9dfff0edc30b28eb8
Prashant Kessa <60229422+Prashant-Kessa@users.noreply.github.com>
2024-09-13 12:39:18 +0200

    admin pages

---
+++ adminHome.js
@@ -0,0 +1,6 @@
+const statsOverview = document.getElementById('card-display');
+
+//onclick go to adminStats.html
+statsOverview.addEventListener('click', function() {
+    window.location.href = "adminStats.html";
+});
