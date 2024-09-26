c6bf3c3d5022d942d0d9eb850d4567a8a77e7851
Prashant Kessa <prashant.kessa@gmail.com>
2024-09-26 14:18:48 +0200

    admin login pages

--- public/adminLogin.js
+++ public/adminLogin.js
@@ -1,25 +1,46 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const form = document.querySelector('form');
-    const emailInput = document.getElementById('email');
-    const passwordInput = document.getElementById('password');
+// export const serverUrl = 'http://localhost:3000';
+// export const clientUrl = 'http://localhost:5001';
 
-    // Hard-coded credentials for now
-    const correctEmail = 'admin@example.com';
-    const correctPassword = 'password123';
+// // Production
+export const serverUrl = 'https://witsgobackend.azurewebsites.net';
+export const clientUrl = 'https://witsgoadmin.azurewebsites.net';
 
-    form.addEventListener('submit', (event) => {
-        event.preventDefault(); // Prevent form submission
+const form = document.querySelector(".form");
 
-        const enteredEmail = emailInput.value;
-        const enteredPassword = passwordInput.value;
+const emailInput = document.querySelector("#email");
+const passwordInput = document.querySelector("#password");
 
-        // Check if the entered credentials are correct
-        if (enteredEmail === correctEmail && enteredPassword === correctPassword) {
-            // Redirect to adminHome.html
-            window.location.href = 'adminHome.html';
-        } else {
-            // Display an error message
-            alert('Incorrect email or password. Please try again.');
+form.addEventListener('submit', async (event) => {
+
+    console.log("SUMBIT")
+    event.preventDefault(); // Prevent form submission
+
+    const enteredEmail = emailInput.value;
+    const enteredPassword = passwordInput.value;
+
+    try {
+        const response = await axios.post(`${serverUrl}/admin/login`, {
+            email: enteredEmail,
+            password: enteredPassword
+        });
+
+        if (response.status === 200) {
+            const token = response.data.accessToken;
+            console.log("token", token);
+            localStorage.setItem('token', token);
+            window.location.href = `${clientUrl}/adminHome.html`;
         }
-    });
+
+
+    } catch (error) {
+        
+        if (error.response.status === 401) {
+            alert('Invalid email or password');
+        } else if (error.response.status === 404) {
+            alert('You are not an admin');
+        }
+
+        console.log(error.message);
+
+    }
 });
