43ddfceeffa0d1770511682ecc9d6478233a2f8d
Alton Maseko <altonmaseko1000@gmail.com>
2024-10-02 20:51:29 +0200

    proper login and logout, friendly urls

--- app.js
+++ app.js
@@ -5,8 +5,22 @@ const app = express()
 // Use the environment's port or default to 3000
 const port = process.env.PORT || 5001
 
+const urlMap = {
+  '/home': '/adminHome.html',
+  '/': '/index.html',
+  '/database': '/databasePage.html',
+};
 // Serve static files from the "public" folder
 app.use(express.static(path.join(__dirname, 'public')))
+// Middleware to handle simplified URLs
+app.get(Object.keys(urlMap), (req, res) => {
+  const actualPath = path.join(__dirname, 'public', urlMap[req.path]);
+  res.sendFile(actualPath, (err) => {
+    if (err) {
+      res.status(404).send('File not found');
+    }
+  });
+});
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
